c14c97949ff02ad4d90a291d0b67d8e4bf2c6dd8
groggovr-droid <groggovr@gmail.com>
2026-07-21 20:52:22 +0300

    Changes saved

---
+++ HW_JS/HW_Lesson_16.js
@@ -0,0 +1,21 @@
+//task 1
+// let x = 20;
+// let y = 58;
+// let z = 42;
+// let sum = x + y + z;
+
+// console.log(`Task 1 result: ${sum}`);
+
+
+//task 2
+
+
+
+
+//task 3
+//task 4
+//task 5
+//task 6
+//task 7
+//task 1+
+//task 2+
---
+++ Lesson 16.js
@@ -0,0 +1,23 @@
+// let name = 'Andrew';
+// let student = name;
+// console.log(name);
+// console.log(student);
+// student = 'Andrew Shcherbo';
+// console.log(student);
+// console.log(student[3]);
+// console.log( student.length );
+
+let text = 'Hello'
+let counter = 100500
+let isOpen = true
+let someValue = null
+
+let typeText = typeof text
+let typeCounter = typeof counter
+let typeIsOpen = typeof isOpen
+let typeSomeValue = typeof someValue
+
+console.log(typeText)
+console.log(typeCounter)
+console.log(typeIsOpen)
+console.log(typeSomeValue)
