d14df15aecaf348c12d972eee2232118c0a3debf
groggovr-droid <groggovr@gmail.com>
2026-07-25 17:37:53 +0300

    Added lesson 17

---
+++ Lesson 17.js
@@ -0,0 +1,17 @@
+// console.log(10 % 3); //остаток от деления
+// console.log(10 ** 3); //возведение в степень
+
+// const x = 7;
+// const y = 3;
+// const z = 4;
+// console.log(x + y + z);
+// console.log(z - x);
+// console.log(x * y);
+// console.log(z ** y);
+// console.log(y % x);
+
+const someString = prompt("Введите текст");
+const result = someString ?? "Default string";
+
+console.log(result);
+// console.log(0 ?? 100);
